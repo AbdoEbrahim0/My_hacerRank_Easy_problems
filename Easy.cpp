846122c85c96fade9af258c76d62ada2fed91929
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2024-12-18 20:43:28 +0200

    solved 3rd in easy
    input :
    3 12345678912345 a 334.23 14049.30493
    output:
    
    3
    12345678912345
    a
    334.230
    14049.304930000

---
+++ Easy.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+
+
+
+
+
+
+    // //https://ask.metafilter.com/307247/Explain-this-exponent
+    // //int a; long b; char c; float f;  double d;
+    // int a; //4 bytes 10 digit number -2,147,483,648 :2,147,483,647
+    // long long  b; // 4 + 4  bytes
+    // char c; //1 byte
+    // float f;  //4byte 
+    // double d; //8bytes
+    // //3 12345678912345 a 334.23 14049.30493
+    // //scanf("%d %ld %c %f %lf", &a ,&b,&c,&f,&d);
+    // // cout<<sizeof(int)<<endl;
+    // // cout<<sizeof(long)<<endl;
+    // // cout<<sizeof(long long)<<endl;
+    // // cout<<sizeof(float)<<endl;
+    // // cout<<sizeof(double)<<endl;
+    // //cout<<"\n naive \n "<<endl;
+    // scanf("%d %lld %c %f %lf", &a ,&b,&c,&f,&d);
+    // //printf("%d %ld %c %f %lf",a ,b,c,f,d);
+    // //cout<<"prefered"<<endl;
+    // printf("%d\n %lld\n %c\n %.3f\n %.9lf\n",a ,b,c,f,d);
+
+    
+    return 0;
+}
