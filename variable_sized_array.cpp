65a3f94fe8ea951144a3172273288f6c69703fc5
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2024-12-24 11:11:14 +0200

    solve issue of not handling sizing of 2d vector
    10 10
    3
    916135 272188 794963
    3
    178997 502468 671251
    1
    122266
    3
    223511 996043 990104
    3
    319694 335208 200789
    2
    867809 273793
    1
    925358
    1
    71140
    1
    862238
    1
    994309
    ----------print mtrix after assign--------
    916135 272188 794963
    178997 502468 671251
    122266                          erRank\Pre
    223511 996043 990104                      pare_cpp>   
    319694 335208 200789
    867809 273793
    925358
    71140
    862238
    994309
    --------
    search for num using i , j :    
    enter indices i , j :
    6 0
    enter indices i , j :
    5 0
    enter indices i , j :
    5 0
    enter indices i , j :
    7 0
    enter indices i , j :
    5 0
    enter indices i , j :
    6 0
    enter indices i , j :
    3 2
    enter indices i , j :
    3 1
    enter indices i , j :
    0 0
    enter indices i , j :
    9 0
    925358
    867809
    867809
    71140
    867809
    925358
    990104
    996043
    916135
    994309

--- variable_sized_array.cpp
+++ variable_sized_array.cpp
@@ -9,76 +9,46 @@ using namespace std;
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     //Vector way
-    //in
-    // 2 2
-    // 3    1 5 4
-    // 5    1 2 8 9 3
-    // 0 1
-    // 1 3
-    //out
-    //5
-    //9
     int n ,q;
     cin>> n>>q;
     int sizeOfSeq;
-    vector<vector<int>> matrix(n, vector<int>(q, 0));
+    vector<vector<int>> matrix(n);
     for (int i=0;i<n;i++)
     {
         // cout<<"enter sizeOfSeq: "<<endl;
-        
         cin>>sizeOfSeq;
+        matrix[i].resize(sizeOfSeq); //expand
         for (int j=0;j<sizeOfSeq;j++)
         {
-            // cout<<"enter element : ";
-            int e;
-            cin>>e;
-            matrix[i][j]=e;
-
+           //  cout<<"enter element : ";
+            int e;  cin>>e; matrix[i][j]=e;
         }
     }
-    cout<<"----------print mtrix after assign--------"<<endl;
-     for (int i=0;i<n;i++)
-    {
-        for (int j=0;j<sizeOfSeq;j++)
-        {
-            cout<<matrix[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
-    // cout<<matrix[0][0]<<" "; //1
-    // cout<<matrix[0][1]<<" "; //2
-    // cout<<"\n";
-    // cout<<matrix[1][0]<<" "; //10
-    // cout<<matrix[1][1]<<" "; //20
-    // cout<<matrix[1][2]<<" "; //30
-    // cout<<"\n";
-    /////////////////////////
-    // cout<<"search for num using i , j :"<<endl;
-    // int ii,jj;
-    // int arr_answers[2];
-    // for (int numOfQ=0;numOfQ<q;numOfQ++)
-    // {
-    // cout<<"enter indices i , j : "<<endl;
-    // cin >>ii>>jj;
-    // cout<<"num is : "<<matrix[ii][jj]<<"\n";
-    // //arr_answers[numOfQ]=matrix[ii][jj];
-    // }
-    // for (int numOfQ=0;numOfQ<q;numOfQ++)
-    // {
-    // cout<<arr_answers[numOfQ]<<endl;
-    // }
-    
-
-    
-    
-    //     for (int i=0;i<n;i++)
+    // cout<<"----------print mtrix after assign--------"<<endl;
+    //  for (int i=0;i<n;i++)
     // {
     //     for (int j=0;j<matrix[i].size();j++)
     //     {
-    //         matrix[i] = vector<int>(q, 0);
+    //         cout<<matrix[i][j]<<" ";
     //     }
+    //     cout<<"\n";
     // }
-
+    // cout<<"--------"<<endl;
+    // cout<<"\n";
+    /////////////////////////
+    // cout<<"search for num using i , j :"<<endl;
+    int ii,jj;
+    int arr_answers[q];
+    for (int numOfQ=0;numOfQ<q;numOfQ++)
+    {
+    // cout<<"enter indices i , j : "<<endl;
+    cin >>ii>>jj;
+    //cout<<"num is : "<<matrix[ii][jj]<<"\n";
+    arr_answers[numOfQ]=matrix[ii][jj];
+    }
+    for (int numOfQ=0;numOfQ<q;numOfQ++)
+    {
+    cout<<arr_answers[numOfQ]<<endl;
+    }
     return 0;
-    
 }
